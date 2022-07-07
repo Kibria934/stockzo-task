d6f0907575c4d1e63cea2851a78d284002c99aed
Kibria934 <kibriahossain725@gmai.com>
2022-07-08 02:48:07 +0600

    login added

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,46 +3,39 @@ import pic4 from "../Assets/Images/pic4.jpg";
 
 const Login = () => {
   return (
-    <div class="hero mt-28 min-h-screen bg-black">
-      <div class="hero-content flex-col lg:flex-row-reverse">
-        <div class="text-center lg:text-left">
-          <h1 class="text-5xl font-bold">Login now!</h1>
-          <p class="py-6">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
-          </p>
-        </div>
-        <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-          <div class="card-body">
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder="email"
-                class="input input-bordered"
-              />
-            </div>
-            <div class="form-control">
-              <label class="label">
-                <span class="label-text">Password</span>
-              </label>
-              <input
-                type="text"
-                placeholder="password"
-                class="input input-bordered"
-              />
-              <label class="label">
-                <a href="#" class="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div class="form-control mt-6">
-              <button class="btn btn-primary">Login</button>
-            </div>
+    <div class="heroSection mt-28 items-center flex w-full h-screen ">
+      <div class=" flex-shrink-0 ml-10 w-96">
+        <div class="">
+          <h1 className="text-5xl py-4 font-bold">
+            Hello! <br /> Good Morning
+          </h1>
+          <p className="text-2xl medium pb-5">Log in your account</p>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text">Username</span>
+            </label>
+            <input
+              type="text"
+              placeholder=""
+              class="bg-transparent border-b-2 border-dashed outline-0"
+            />
+          </div>
+          <div class="form-control">
+            <label class="label">
+              <span class="label-text float-right">Password</span>
+            </label>
+            <input
+              type="text"
+              class="bg-transparent border-b-2 border-dashed outline-0"
+            />
+            <label class="label">
+              <a href="#" class="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div class="form-control mt-6">
+            <button class="btn bg-black">Login</button>
           </div>
         </div>
       </div>
